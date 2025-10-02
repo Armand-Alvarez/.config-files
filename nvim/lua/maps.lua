4efa590aa8df5580688f4c8692563c845065f1f3
Armand-Alvarez <email@armandalvarez.com>
2025-10-02 17:00:39 -0400

    Added mapping to run the current python file in a terminal split below. Removed lower "--" below each section to avoid unecessary filler

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -12,16 +12,22 @@ map("n", "<leader>q", "<CMD>q<CR>")
 
 -- Exit insert mode
 map("i", "jk", "<ESC>")
---
 
 -- Exit terminal mode
 map("t", "<Esc>", "<C-\\><C-n>")
---
 
 -- New Windows
 map("n", "<leader>o", "<CMD>vsplit<CR>")
 map("n", "<leader>p", "<CMD>split<CR>")
---
+
+-- Run current Python file in a terminal split below
+map("n", "<leader>rf", function()
+	-- Save the file first (optional)
+	vim.cmd("write")
+
+	-- Open a split terminal below and run the file
+	vim.cmd("belowright split | terminal python3 %")
+end)
 
 -- Window Navigation
 map("n", "<C-h>", "<C-w>h")
@@ -34,11 +40,9 @@ map("n", "<C-Left>", "<C-w><")
 map("n", "<C-Right>", "<C-w>>")
 map("n", "<C-Up>", "<C-w>+")
 map("n", "<C-Down>", "<C-w>-")
---
 
 -- NeoTree
 map("n", "<leader>tt", "<CMD>Neotree toggle<CR>")
---
 
 -- Telescope
 map("n", "<leader>ff", "<cmd>Telescope find_files<cr>") --{ desc = "Fuzzy find files in cwd" })
@@ -46,8 +50,6 @@ map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>") --{ desc = "Fuzzy find re
 map("n", "<leader>fb", "<cmd>Telescope buffers<cr>") --{ desc = "Find string in cwd" })
 map("n", "<leader>fs", "<cmd>Telescope git_status<cr>") --{ desc = "Find string under cursor in cwd" })
 map("n", "<leader>fc", "<cmd>Telescope git commits<cr>") --{ desc = "Find todos" })
---
 
 -- Neogit
 map("n", "<leader>gg", "<cmd>Neogit kind=floating<CR>")
---
