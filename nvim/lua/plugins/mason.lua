09ee24b83f88149a8f3a147076d7bd15461cb53b
Armand Alvarez <aralvarez159@gmail.com>
2025-10-11 17:57:33 -0400

    Configured mason to use pyright

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,4 +1,20 @@
 return {
-    "mason-org/mason.nvim",
-    opts = {}
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+	},
+	config = function()
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+
+		require("mason").setup()
+		local mason_lspconfig = require("mason-lspconfig")
+		mason_lspconfig.setup({
+			ensure_installed = { "pyright" },
+		})
+		vim.lsp.config("pyright", {
+			capabilities = capabilities,
+		})
+	end,
 }
