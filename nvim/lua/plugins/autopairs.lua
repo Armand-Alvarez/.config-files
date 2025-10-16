a7eb24b10294e8b103d79b2ac6633d48cc7b2d54
Armand Alvarez <aralvarez159@gmail.com>
2025-10-15 21:27:47 -0400

    Removed existing options, now only have option to not add a close if adding an open when they all match up

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -1,13 +1,8 @@
 return {
 	"windwp/nvim-autopairs",
 	event = "InsertEnter",
-
-	opts = {
-		disable_filetype = { "TelescopePrompt", "vim" },
-		map_cr = false,
-	},
-
-	config = function(_, opts)
-		require("nvim-autopairs").setup(opts)
-	end,
+    config = true,
+    opts = {
+        enable_check_bracket_line = false,
+    }
 }
