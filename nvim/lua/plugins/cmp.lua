d204697cde9b4dfb007ac4cf7e938dd20b407212
Armand Alvarez <aralvarez159@gmail.com>
2025-10-15 21:28:00 -0400

    Added autopairs rules for cmp

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -14,6 +14,44 @@ return {
 		local cmp = require("cmp")
 		local luasnip = require("luasnip")
 		local lspkind = require("lspkind")
+        local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+        local handlers = require('nvim-autopairs.completion.handlers')
+
+        cmp.event:on(
+          'confirm_done',
+          cmp_autopairs.on_confirm_done({
+            filetypes = {
+              -- "*" is a alias to all filetypes
+              ["*"] = {
+                ["("] = {
+                  kind = {
+                    cmp.lsp.CompletionItemKind.Function,
+                    cmp.lsp.CompletionItemKind.Method,
+                  },
+                  handler = handlers["*"]
+                }
+              },
+              lua = {
+                ["("] = {
+                  kind = {
+                    cmp.lsp.CompletionItemKind.Function,
+                    cmp.lsp.CompletionItemKind.Method
+                  },
+                  ---@param char string
+                  ---@param item table item completion
+                  ---@param bufnr number buffer number
+                  ---@param rules table
+                  ---@param commit_character table<string>
+                  handler = function(char, item, bufnr, rules, commit_character)
+                    -- Your handler function. Inspect with print(vim.inspect{char, item, bufnr, rules, commit_character})
+                  end
+                }
+              },
+              -- Disable for tex
+              tex = false
+            }
+          })
+        )
 
 		require("luasnip.loaders.from_vscode").lazy_load()
 
