c6f85f65361a659a01cf01835d09675581f2ba27
Armand Alvarez <aralvarez159@gmail.com>
2025-10-18 18:45:33 -0400

    Added keymap for telescope neoclip

--- nvim/lua/plugins/neoclip.lua
+++ nvim/lua/plugins/neoclip.lua
@@ -4,5 +4,10 @@ return {
         {'nvim-telescope/telescope.nvim'},
         {'kkharji/sqlite.lua', module = 'sqlite'}, -- Include this for persistent history between sessions
     },
-    opts = {}
+    opts = {
+        history = 100,
+        vim.keymap.set('n', '<Leader>p', function()
+            vim.cmd('Telescope neoclip')
+        end)
+    }
 }
