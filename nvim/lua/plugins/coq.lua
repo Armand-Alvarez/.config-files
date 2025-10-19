5f6da8693e963dc156bc4b382262acad8e89101d
Armand Alvarez <aralvarez159@gmail.com>
2025-10-19 15:24:18 -0400

    Make coq quiet at start

--- nvim/lua/plugins/coq.lua
+++ nvim/lua/plugins/coq.lua
@@ -19,7 +19,7 @@ return {
   },
   init = function()
     vim.g.coq_settings = {
-        auto_start = true, -- if you want to start COQ at startup
+        auto_start = "shut-up", -- if you want to start COQ at startup (and shut the fuck up)
         -- Your COQ settings here
     }
   end,
