e8263384734468204af9f55bb3346e1fe3a51e07
Armand-Alvarez <email@armandalvarez.com>
2025-10-26 00:01:12 -0400

    Adjusted blink and copilot plugins to work better together

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,10 +1,14 @@
 return {
     "github/copilot.vim",
-    "CopilotC-Nvim/CopilotChat.nvim",
-    build = "make tiktoken",
-    dependencies = {
-      { "nvim-lua/plenary.nvim", branch = "master" },
-    },
+    -- Load this before config so the variable is set early
+    init = function()
+        vim.g.copilot_no_tab_map = true
+    end,
     config = function()
+        -- Optional: map <C-J> for Copilot accept
+        vim.keymap.set('i', '<C-J>', 'copilot#Accept("<CR>")', {
+            expr = true,
+            replace_keycodes = false
+        })
     end,
 }
