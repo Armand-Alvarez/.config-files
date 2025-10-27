d441a84c1a1cf16f4c508d5bc72a2d88bcb368d6
Armand-Alvarez <email@armandalvarez.com>
2025-10-26 20:24:22 -0400

    Fixed keymaps to work for toggleterm

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ TODO:
 - [x] Show copilot suggestions while also seeing COQ completions
 - [x] Add git diff within nvim (using gitsigns or similar)
 - [x] Remove redundant keybinds
-- [ ] Fix toggleterm to escape from the terminal with <esc>
+- [x] Fix toggleterm to escape from the terminal with <esc>
--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -49,8 +49,8 @@ function M.setup()
     end, { desc = 'Toggle diagnostic virtual_lines' })
 
     -- 🔹 ToggleTerm Keymap (~)
-    function M.set_toggleterm_keymaps(term_bufnr)
-        local opts = { buffer = term_bufnr, silent = true }
+    function _G.set_terminal_keymaps()
+        local opts = {buffer = 0}
         vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
         vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
         vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
@@ -59,6 +59,9 @@ function M.setup()
         vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
         vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
     end
+
+    -- if you only want these mappings for toggle term use term://*toggleterm#* instead
+    vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
 
 
