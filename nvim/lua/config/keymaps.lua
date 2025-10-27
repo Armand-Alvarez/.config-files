7e042c6618d98a4bde2d9b2708e68c929159f9ee
Armand-Alvarez <email@armandalvarez.com>
2025-10-26 20:07:10 -0400

    Changed split window keybind for nvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -29,8 +29,8 @@ function M.setup()
     keymap.set("n", "<ESC>", ":nohl<CR>")
     --
     -- window management
-    keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
-    keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+    keymap.set("n", "<leader>sl", "<C-w>v") -- split window vertically
+    keymap.set("n", "<leader>sj", "<C-w>s") -- split window horizontally
     keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
     keymap.set("n", "<leader>qq", ":close<CR>") -- close current split window
     keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
