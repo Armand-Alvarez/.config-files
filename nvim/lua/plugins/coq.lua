771cbbbcad5f9cf6aec08ff1ca6a41c8e2356f58
Armand Alvarez <aralvarez159@gmail.com>
2025-10-30 20:22:45 -0400

    Enhanced coq plugin
    * Removed redundant portions
    * Added copilot accepted suggestions

--- nvim/lua/plugins/coq.lua
+++ nvim/lua/plugins/coq.lua
@@ -1,29 +1,28 @@
 return {
-  "neovim/nvim-lspconfig", -- REQUIRED: for native Neovim LSP integration
-  lazy = false, -- REQUIRED: tell lazy.nvim to start this plugin at startup
+  "neovim/nvim-lspconfig",
+  lazy = false,
   dependencies = {
-    -- main one
     { "ms-jpq/coq_nvim", branch = "coq" },
-
-    -- 9000+ Snippets
     { "ms-jpq/coq.artifacts", branch = "artifacts" },
-
-    -- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
-    -- Need to **configure separately**
-    { 'ms-jpq/coq.thirdparty', branch = "3p" }
-    -- - shell repl
-    -- - nvim lua api
-    -- - scientific calculator
-    -- - comment banner
-    -- - etc
+    { "ms-jpq/coq.thirdparty", branch = "3p" },
   },
   init = function()
     vim.g.coq_settings = {
-        auto_start = "shut-up", -- if you want to start COQ at startup (and shut the fuck up)
-        -- Your COQ settings here
+      auto_start = "shut-up",
     }
   end,
   config = function()
-    -- Your LSP settings here
+    -- Load coq
+    local coq = require("coq")
+
+    -- Load 3rd party sources
+    require("coq_3p") {
+      { src = "copilot", short_name = "COP", accept_key = "<C-J>" },
+    }
+
+    -- Example: attach LSP with COQ
+    local lsp = require("lspconfig")
+    lsp.lua_ls.setup(coq.lsp_ensure_capabilities({}))
   end,
 }
+
